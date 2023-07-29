be7c5d4a6ff699bba3e55b76f14ac6be6640858f
thaliaraujo <thalia.araujo321@gmail.com>
2023-07-29 17:41:25 -0300

    commit

--- desafio5-6.js
+++ desafio5-6.js
@@ -1,91 +1,138 @@
-// Lógica JS 5/7: Arrays e coleções
-// Lógica JS 6/7: Remoção de Arrays
-
-let frutas = [];
-let verduras = [];
-let laticinios = [];
-let proteinas = [];
-let carboidratos = [];
-let bebidas = [];
-let limpeza = [];
-let outros = [];
-let produtos = '';
-let categorias = '';
-
-let addProduto = 'sim';
-while (addProduto != 'não') {
-    if(frutas.length === 0 && verduras.length === 0 && laticinios.length === 0 && proteinas.length === 0 && carboidratos.length === 0 && bebidas.length === 0 && limpeza.length === 0 && outros.length === 0) {
-        addProduto = prompt('Você deseja adicionar um produto na lista de compras? responda "sim" ou "não"').toLowerCase();
-    } else {
-        addProduto = prompt('Você deseja adicionar ou remover um produto da lista de compras? responda "sim", "não" ou "remover"').toLowerCase();
+// Criando um objeto para armazenar as categorias e os produtos da lista de compras
+let listaDeCompras = {
+    frutas: [],
+    verduras: [],
+    laticinios: [],
+    proteinas: [],
+    carboidratos: [],
+    bebidas: [],
+    limpeza: [],
+    outros: []
+  };
+  
+// Criando uma função para adicionar um produto na lista
+function adicionarProduto() {
+    while(true) {
+        let produtos = prompt("Qual produto deseja adicionar?");
+        let categorias = prompt('Em qual categoria o produto se encaixa? "frutas", "Verduras", "laticinios", "proteinas", "carboidratos", "bebidas", "limpeza", "outros".').toLowerCase();
+        // Usando um switch case para verificar a categoria e adicionar o produto na lista
+        switch (categorias) {
+            case "frutas":
+                listaDeCompras.frutas.push(produtos);
+                break;
+            case "verduras":
+                listaDeCompras.verduras.push(produtos);
+                break;
+            case "laticinios":
+                listaDeCompras.laticinios.push(produtos);
+                break;
+            case "proteinas":
+                listaDeCompras.proteinas.push(produtos);
+                break;
+            case "carboidratos":
+                listaDeCompras.carboidratos.push(produtos);
+                break;
+            case "bebidas":
+                listaDeCompras.bebidas.push(produtos);
+                break;
+            case "limpeza":
+                listaDeCompras.limpeza.push(produtos);
+                break;
+            case "outros":
+                listaDeCompras.outros.push(produtos);
+                break;
+            default:
+                alert("Essa categoria não foi pré-definida, tente novamente!");
+                continue;
+        };
+        return categorias;
     };
-
-    while (addProduto != 'sim' && addProduto != 'não' && addProduto != 'remover') {
-        alert('Operação invalida! Tente novamente.');
-        addProduto = prompt('Você deseja adicionar ou remover um produto da lista de compras? responda "sim", "não" ou "remover"').toLowerCase();
-   };
-
-   if(addProduto === 'não') {
-    break;
-   };
-
-   if (addProduto === 'sim') {
-    produtos = prompt('Qual produto deseja adicionar?');
-    categorias = prompt('Em qual categoria o produto se encaixa? "frutas", "Verduras", "laticinios", "proteinas", "carboidratos", "bebidas", "limpeza", "outros".').toLowerCase();
-    
-        if (categorias === 'frutas') {
-            frutas.push(produtos)
-        } else if (categorias === 'verduras') {
-            verduras.push(produtos)
-        } else if (categorias === 'laticinios') {
-            laticinios.push(produtos)
-        } else if (categorias === 'proteinas') {
-            proteinas.push(produtos)
-        } else if (categorias === 'carboidratos') {
-            carboidratos.push(produtos)
-        } else if (categorias === 'bebidas') {
-            bebidas.push(produtos)
-        } else if (categorias === 'limpeza') {
-            limpeza.push(produtos)
-        } else if (categorias === 'outros') {
-            outros.push(produtos)
-        } else {
-            alert('Essa categoria não foi pré-definida, o produto não será adicionado na lista.')
-        }
-    } else if (addProduto === 'remover') {
-        if(frutas.length === 0 && verduras.length === 0 && laticinios.length === 0 && proteinas.length === 0 && carboidratos.length === 0 && bebidas.length === 0 && limpeza.length === 0 && outros.length === 0) {
-            alert('Lista de compras vazia.');
-        } else {
-            remover = prompt(`Lista de Compras:\n Frutas: ${frutas}\n Verduras: ${verduras}\n Laticinios: ${laticinios}\n Proteinas: ${proteinas}\n Carboidratos: ${carboidratos}\n Bebidas: ${bebidas}\n Limpeza: ${limpeza}\n Outros: ${outros}\n Qual produto você deseja remover?`);
-            if(frutas.indexOf(remover) != -1){
-                frutas.splice(frutas.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(verduras.indexOf(remover) != -1){
-                verduras.splice(verduras.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(laticinios.indexOf(remover) != -1){
-                laticinios.splice(laticinios.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(proteinas.indexOf(remover) != -1){
-                proteinas.splice(proteinas.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(carboidratos.indexOf(remover) != -1){
-                carboidratos.splice(carboidratos.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(bebidas.indexOf(remover) != -1){
-                bebidas.splice(bebidas.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(limpeza.indexOf(remover) != -1){
-                limpeza.splice(limpeza.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else if(outros.indexOf(remover) != -1){
-                outros.splice(outros.indexOf(remover), 1);
-                alert(`O item ${remover} foi removido com sucesso!`)
-            } else {
-                alert(`Não foi possível encontrar o item dentro da lista!`)
-            }
-        }
-    }
 };
+  
+// Criando uma função para remover um produto da lista
+function removerProduto() {
+    // Verificando se a lista está vazia
+    if (
+      listaDeCompras.frutas.length === 0 &&
+      listaDeCompras.verduras.length === 0 &&
+      listaDeCompras.laticinios.length === 0 &&
+      listaDeCompras.proteinas.length === 0 &&
+      listaDeCompras.carboidratos.length === 0 &&
+      listaDeCompras.bebidas.length === 0 &&
+      listaDeCompras.limpeza.length === 0 &&
+      listaDeCompras.outros.length === 0
+    ) {
+      alert("Lista de compras vazia.");
+    } else {
+        // Pedindo ao usuário o nome do produto que ele quer remover
+        let remover = prompt(
+            `Lista de Compras:\n Frutas: ${listaDeCompras.frutas}\n Verduras: ${listaDeCompras.verduras}\n Laticinios: ${listaDeCompras.laticinios}\n Proteinas: ${listaDeCompras.proteinas}\n Carboidratos: ${listaDeCompras.carboidratos}\n Bebidas: ${listaDeCompras.bebidas}\n Limpeza: ${listaDeCompras.limpeza}\n Outros: ${listaDeCompras.outros}\n Qual produto você deseja remover?`
+        );
+        // Usando um loop for para percorrer as categorias da lista
+        for (let categoria in listaDeCompras) {
+            // Verificando se o produto está na categoria
+            if (listaDeCompras[categoria].indexOf(remover) != -1) {
+            // Removendo o produto da categoria
+            listaDeCompras[categoria].splice(listaDeCompras[categoria].indexOf(remover), 1);
+            alert(`O item ${remover} foi removido com sucesso!`);
+            return; // Saindo da função depois de remover o produto
+        };
+      };
+      // Se o produto não foi encontrado em nenhuma categoria, avisar ao usuário
+      alert(`O item ${remover} não está na lista de compras.`);
+    };
+  };
+  
+// Criando uma função para mostrar a lista de compras na tela
+function mostrarLista() {
+    // Verificando se a lista está vazia
+    if (
+        listaDeCompras.frutas.length === 0 &&
+        listaDeCompras.verduras.length === 0 &&
+        listaDeCompras.laticinios.length === 0 &&
+        listaDeCompras.proteinas.length === 0 &&
+        listaDeCompras.carboidratos.length === 0 &&
+        listaDeCompras.bebidas.length === 0 &&
+        listaDeCompras.limpeza.length === 0 &&
+        listaDeCompras.outros.length === 0
+    ) {
+        alert("Lista de compras vazia.");
+    } else {
+        // Criando uma variável para armazenar a lista formatada
+        let listaFormatada = "Lista de Compras:\n";
+        // Usando um loop for para percorrer as categorias da lista
+        for (let categoria in listaDeCompras) {
+            // Adicionando a categoria e os produtos na lista formatada
+            listaFormatada += `${categoria}: ${listaDeCompras[categoria].join(", ")}\n`;
+        }
+        // Mostrando a lista formatada na tela
+        alert(listaFormatada);
+    };
+};
+  
+// Criando uma variável para armazenar a opção do usuário
+let opcao = "";
 
-alert(`Lista de Compras:\n Frutas: ${frutas}\n Verduras: ${verduras}\n Laticinios: ${laticinios}\n Proteinas: ${proteinas}\n Carboidratos: ${carboidratos}\n Bebidas: ${bebidas}\n Limpeza: ${limpeza}\n Outros: ${outros}\n`);
+// Criando um loop while para repetir o programa até o usuário sair
+while (opcao != "sair") {
+    // Pedindo ao usuário a opção que ele quer executar
+    opcao = prompt('O que você deseja fazer? Digite "adicionar", "remover", "mostrar" ou "sair".').toLowerCase();
+    // Usando um switch case para verificar a opção do usuário e chamar a função correspondente
+    switch (opcao) {
+        case "adicionar":
+            adicionarProduto();
+            break;
+        case "remover":
+            removerProduto();
+            break;
+        case "mostrar":
+            mostrarLista();
+            break;
+        case "sair":
+            alert("Obrigado por usar o programa. Até mais!");
+            break;
+        default:
+            alert("Opção inválida. Tente novamente.");
+    };
+};
+  
